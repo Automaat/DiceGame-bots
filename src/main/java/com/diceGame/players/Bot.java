adacd7fa014736835bce598140989453a1c0e670
Automaat <marcin0skalski@gmail.com>
2015-11-23 20:50:22 +0100

    removed template comments

--- src/main/java/com/diceGame/players/Bot.java
+++ src/main/java/com/diceGame/players/Bot.java
@@ -1,14 +1,4 @@
-package com.diceGame.players;/*
- * Copyright (c) 2015 by Sabre Holdings Corp.
- * 3150 Sabre Drive, Southlake, TX 76092 USA
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of Sabre Holdings Corporation ("Confidential Information").
- * You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement
- * you entered into with Sabre Holdings Corporation.
- */
+package com.diceGame.players;
 
 import com.diceGame.Decision;
 import com.diceGame.GameContext;
